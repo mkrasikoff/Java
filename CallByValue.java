ab2aa91b9c7f83f64c4f1c5a29f418918b627fe1
Mikhail Krasikov <corolish2@gmail.com>
2020-10-04 10:13:53 +0300

    4 october

---
+++ CallByValue.java
@@ -0,0 +1,15 @@
+class Test {
+	void noChange(int i, int j) {
+		i = i + j;
+		j = -j;
+	}
+}
+class CallByValue {
+	public static void main(String[] args) {
+		Test ob = new Test();
+		int a = 15, b = 20;
+		System.out.println("a и b перед вызовом: " + a + " " + b);
+		ob.noChange(a, b);
+		System.out.println("a и b после вызова: " + a + " " + b);
+	}
+}
