f0fc5237ec015f74d8359cdfe79b8a196695f06c
Mikhail Krasikov <corolish2@gmail.com>
2020-10-12 13:14:01 +0300

    12 october

---
+++ CharArithDemo.java
@@ -0,0 +1,11 @@
+class CharArithDemo {
+	public static void main(String[] args) {
+		char ch;
+		ch = 'X';
+		System.out.println("ch содержит " + ch);
+		ch++;
+		System.out.println("теперь ch содержит " + ch);
+		ch = 98;
+		System.out.println("теперь ch содержит " + ch);
+	}
+}
