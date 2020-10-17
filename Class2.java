5cfac5a366368a9b96329142714596f37c81412f
Mikhail Krasikov <corolish2@gmail.com>
2020-10-17 23:36:49 +0300

    17 october

---
+++ Class2.java
@@ -0,0 +1,21 @@
+class Class1 { 
+   Class1(int i) { 
+       System.out.println("Class1(int)"); 
+   } 
+} 
+ 
+public class Class2 extends Class1 { 
+   Class2(double d) {              // 1 
+       this((int) d); 
+       System.out.println("Class2(double)"); 
+   } 
+ 
+   Class2(int i) {    
+       this(double i);             // 2 
+       System.out.println("Class2(int)"); 
+   } 
+ 
+   public static void main(String[] args) { 
+       new Class2(0.0); 
+   } 
+}
