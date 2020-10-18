9a9b5032f1f130631463549831c95e93b4dc029c
Mikhail Krasikov <corolish2@gmail.com>
2020-10-18 13:27:11 +0300

    18 october

---
+++ ClassB.java
@@ -0,0 +1,18 @@
+class ClassA {
+	private final String value;
+	public ClassA(String value) {
+		this.value = value;
+		System.out.println();
+	}
+}
+
+public class ClassB extends ClassA {
+	private final String value = new String();
+	public ClassB() {
+		super("EPAM");
+	}
+	public static void main(String[] args) {
+		ClassA var = new ClassB();
+		System.out.println(var);
+	}
+}
