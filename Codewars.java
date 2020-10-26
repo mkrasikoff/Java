48896e6ea46cd1e947a4ade672bf0b808876e234
Mikhail Krasikov <corolish2@gmail.com>
2020-10-26 18:18:36 +0300

    26 october

---
+++ Codewars.java
@@ -0,0 +1,22 @@
+public class Codewars {
+	public static String oddOrEven (int[] array) {
+		int sum = 0;
+		for(int x: array) {
+			sum += x;
+		}
+
+		if((sum % 2) == 0) {
+			return "even";
+		}
+		else if (sum == 0) {
+			return "even";
+		}
+		else return "odd";
+	}
+
+	public static void main(String[] args) {
+
+		int[] testArray = { 2, 2, 5, 4 };
+		System.out.println(new Codewars().oddOrEven(testArray));
+	}
+}
