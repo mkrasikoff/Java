791fd3ab86ab665ffcd82313920e45c85b69f6f2
Mikhail Krasikov <corolish2@gmail.com>
2020-10-31 19:21:37 +0300

    31 october

---
+++ Compare.java
@@ -0,0 +1,12 @@
+public class Compare { 
+   public static void main(String[] args) { 
+       String s1 = new String("Hello"); 
+       String s2 = new String("Hello"); 
+ 
+       if (s1.equals(s2)) { 
+           System.out.println("True"); 
+       } else { 
+           System.out.println("False"); 
+       } 
+   } 
+} 
