d7f5f46c6bf56d28ff52c8bb61e674e871081bea
Mikhail Krasikov <corolish2@gmail.com>
2020-11-08 20:39:56 +0300

    8 november

---
+++ ContDemo.java
@@ -0,0 +1,9 @@
+class ContDemo {
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i <= 100; i++) {
+			if ((i % 2) != 0) continue; // завершить шаг итерации цикла
+			System.out.println(i);
+		}
+	}
+}
