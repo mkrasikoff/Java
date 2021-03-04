721691a4855fb6af80670b2702a302e10dadf269
Mikhail Krasikov <corolish2@gmail.com>
2021-03-04 23:58:32 +0300

    4 march

---
+++ Echo.java
@@ -0,0 +1,8 @@
+class Echo {
+	public static void main(String[] args) {
+		double time, distance;
+		time = 6.3;
+		distance = 1100 * (time / 2);
+		System.out.println("Расстояние от меня до стены в пещере по времени эхо равно: " + distance + " футов");
+	}
+	}
