005c183233070f4d3d8c23e296a63deeb70fac95
Mikhail Krasikov <corolish2@gmail.com>
2021-05-07 00:07:04 +0300

    7 may

---
+++ EnumDemo2.java
@@ -0,0 +1,20 @@
+enum Transport {
+	CAR, TRUCK, AIRPLANE, TRAIN, BOAT
+}
+
+class EnumDemo2 {
+	public static void main(String[] args) {
+		Transport tp;
+
+		System.out.println("Константы Transport: ");
+
+		Transport allTransports[] = Transport.values();
+
+		for(Transport t: allTransports) System.out.println(t);
+
+		System.out.println();
+
+		tp = Transport.valueOf("AIRPLANE");
+		System.out.println("tp содержит " + tp);
+	}
+}
