e8727b4274ba651e11484effb80e7ab4533b488c
Mikhail Krasikov <corolish2@gmail.com>
2021-05-29 10:01:13 +0300

    29 may

---
+++ ErrInfo.java
@@ -0,0 +1,33 @@
+class Err {
+	String msg; // сообщение об ошибке
+	int severity; // уровень серьезности ошибки
+	Err(String m, int s) {
+		msg = m;
+		severity = s;
+	}
+}
+class ErrorInfo {
+	String msgs[] = {
+		"Ошибка вывода",
+		"Ошибка ввода",
+		"Отсутствует место на диске",
+		"Выход индекса за границе диапазона"
+	};
+	int howbad[] = { 3, 3, 2, 4};
+	Err getErrorInfo(int i) {
+		if(i >= 0 & i < msgs.length)
+			return new Err(msgs[i], howbad[i]);
+		else
+			return new Err("Несуществующий код ошибки", 0);
+	}
+}
+class ErrInfo {
+	public static void main(String[] args) {
+		ErrorInfo err = new ErrorInfo();
+		Err e;
+		e = err.getErrorInfo(2);
+		System.out.println(e.msg + " уровень: " + e.severity);
+		e = err.getErrorInfo(19);
+		System.out.println(e.msg + " уровень: " + e.severity);
+	}
+}
