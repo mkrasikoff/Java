6bb944992c36825720a09fc768346eae3f8ba8dc
Mikhail Krasikov <mikhail_krasikov@epam.com>
2021-05-30 11:19:40 +0300

    30 may

---
+++ ErrMsg.java
@@ -0,0 +1,21 @@
+class ErrorMsg {
+	String msgs[] = {
+		"Ошибка вывода",
+		"Ошибка ввода",
+		"Отсутствует место на диске",
+		"Выход индекса за границы диапазона"
+	};
+	String getErrorMsg(int i) {
+		if(i >= 0 & i < msgs.length)
+			return msgs[i];
+		else
+			return "Несуществующий код ошибки";
+	}
+}
+class ErrMsg {
+	public static void main(String[] args) {
+		ErrorMsg err = new ErrorMsg();
+		System.out.println(err.getErrorMsg(2));
+		System.out.println(err.getErrorMsg(19));
+	}
+}
