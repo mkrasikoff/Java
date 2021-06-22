6074abb603a595de74873e8dda9df0d69f811a3d
Mikhail Krasikov <mikhail_krasikov@epam.com>
2021-06-22 20:48:20 +0300

    22 june

---
+++ ReadBytes.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class ReadBytes {
+	public static void main(String[] args) throws IOException {
+		byte data[] = new byte[10];
+		System.out.println("Введите символы.");
+		System.in.read(data);
+		System.out.print("Вы ввели: ");
+		for(int i = 0; i < data.length; i++) System.out.print((char) data[i]);
+	}
+}
