4d1f53890c3b7a7f4a2aaf750f95002b89a869cc
Mikhail Krasikov <mikhail_krasikov@epam.com>
2021-10-21 17:38:22 +0300

    21 october

--- ClassicalMusic.java
+++ ClassicalMusic.java
@@ -1,16 +1,20 @@
 package ru.mkrasikoff.springproject;
 
 public class ClassicalMusic implements Music {
+	
     @Override
     public String getSong() {
+		
         return "Hungarian Rapsody";
     }
 
     public void init() {
+		
         System.out.println("doing my initialization in classicalmusic");
     }
 
     public void destroy() {
+		
         System.out.println("doing my destroying in classicalmusic");
     }
 }
