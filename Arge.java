3a7b1b1a576254299a8a49eb57dab9ab98bafded
Mikhail Krasikov <mikhail_krasikov@epam.com>
2021-10-31 19:16:09 +0300

    31 october

--- Arge.java
+++ Arge.java
@@ -1,17 +1,21 @@
 class Arge {
     
     public static int nbYear(int p0, double percent, int aug, int p) {
+		
         int result = p0;
         int yearsCount = 0;
+		
         while(result < p) {
         	int growth = (int) ((double) result * (percent / 100)) + aug;
         	result += growth;
         	yearsCount++;
         }
+		
         return yearsCount;
     }
 
     public static void main(String[] args) {
+		
     	System.out.println(nbYear(1500, 5, 100, 5000)); // need 15
     	System.out.println(nbYear(1_500_000, 2.5, 10_000, 2_000_000)); // need 10
     	System.out.println(nbYear(1_500_000, 0.25, 1000, 2_000_000)); // need 94
