60cb4b6deeb4970c7a10b156ad09cc812706befc
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-03-30 17:16:45 +0300

    30 march

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -13,11 +13,23 @@ class AddTwoNumbers {
         
         List<Integer> listWithResult = new ArrayList<>();
         
-        while(l1 != null) {
+        boolean addToNextInt = false;
+        
+        while(l1 != null || l2 != null || addToNextInt == true) {
             int firstNodeValue = getValueFromNode(l1);
             int secondNodeValue = getValueFromNode(l2);
-            
             int sum = firstNodeValue + secondNodeValue;
+            
+            if(addToNextInt) {
+                sum += 1;
+                addToNextInt = false;
+            }
+            
+            if(sum >= 10) {
+                addToNextInt = true;
+                sum = sum % 10;
+            }
+            
             listWithResult.add(sum);
             
             l1 = getNextNode(l1);
@@ -25,15 +37,32 @@ class AddTwoNumbers {
         }
         
         System.out.println(listWithResult);
+        ListNode listNode = null;
+        ListNode firstNode = null;
         
-        return null;
+        for(Integer value: listWithResult) {
+            if(firstNode == null) {
+                firstNode = new ListNode(value);
+                listNode = firstNode;
+                continue;
+            }
+            
+            listNode.next = new ListNode(value);
+            listNode = listNode.next;
+        }
+        
+        return firstNode;
     }
     
     private int getValueFromNode(ListNode listNode) {
+        if(listNode == null) return 0;
+        
         return listNode.val;
     }
     
     private ListNode getNextNode(ListNode listNode) {
+        if(listNode == null) return null;
+        
         return listNode.next;
     }
 }
