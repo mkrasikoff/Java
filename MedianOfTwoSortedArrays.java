8e55ac08e918326fc8c960ba74abf165758fb073
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-04-20 12:15:56 +0300

    20 april

---
+++ MedianOfTwoSortedArrays.java
@@ -0,0 +1,41 @@
+class MedianOfTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+      /*
+      Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+      The overall run time complexity should be O(log (m+n)) = 0(log (nums1.size() + nums2.size()))
+       */
+
+      // PREPARING //
+      List<Integer> listWithNumbers = new ArrayList<>();
+      Double result;
+      for(int number: nums1) {
+         listWithNumbers.add(number);
+      }
+      for(int number: nums2) {
+         listWithNumbers.add(number);
+      }
+      Collections.sort(listWithNumbers);
+
+      int listSize = listWithNumbers.size();
+
+      System.out.println(listWithNumbers);
+      // PREPARING END //
+
+      // ALGORITHM //
+      if(listSize == 1) { // case size 1
+         return (double) listWithNumbers.get(0);
+      }
+      if(listSize % 2 == 1) { // EVEN size case
+         result = (double) (listWithNumbers.get(listSize / 2));
+      }
+      else { // ODD size case
+         result = (((double) listWithNumbers.get(listSize / 2 - 1))
+                 + ((double) listWithNumbers.get(listSize / 2))) / 2;
+      }
+      // END OF ALGORITHM //
+
+      return result;
+   }
+}
