865563166e9b12b5656a0509ccfcbd874b46a1d2
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-04-21 18:13:22 +0300

    21 april

---
+++ IntegerToRomanSimple.java
@@ -0,0 +1,10 @@
+class IntegerToRomanSimple {
+    public String intToRoman(int num) {
+      String[] M = new String[] {"", "M", "MM", "MMM"};
+      String[] C = new String[] {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+      String[] X = new String[] {"", "X", "XX", "XXX", "LX", "L", "LX", "LXX", "LXXX", "LC"};
+      String[] I = new String[] {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+      return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[(num % 10)];
+   }
+}
