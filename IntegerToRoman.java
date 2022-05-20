ec44702d18051d4c64e9d402c0828146c2565ed4
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-05-20 09:43:47 +0300

    20 may

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -1,5 +1,6 @@
 class IntegerToRoman {
     public String intToRoman(int num) {
+		
       StringBuilder stringBuilder = new StringBuilder();
       String[] romans = new String[] { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
       int[] ints = new int[] { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
@@ -18,7 +19,7 @@ class IntegerToRoman {
       for (int i = ints.length - 1; i >= 0; i--) {
          times = num / ints[i];
          num %= ints[i];
-
+		
          while (times > 0) {
             stringBuilder.append(romans[i]);
             times--;
